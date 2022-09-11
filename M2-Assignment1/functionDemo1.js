47fc919ef6de0219b0c6ef69976028ce3659d930
Shradsk <102869564+Shradsk@users.noreply.github.com>
2022-09-10 17:28:29 -0700

    Add files via upload

---
+++ M2-Assignment1/functionDemo1.js
@@ -0,0 +1,23 @@
+var age = 10; //int
+var salary = 15.00; //double
+
+// console.log(`The variable type of age is ${typeof age}`);
+// console.log(`The variable type of salary is ${typeof salary}`);
+
+// var name = 'John Smith';
+// console.log(`The variable type of name is ${typeof name}`);
+
+const displayGreetings = function(name, year){
+    console.log(`Happy new year ${year} ${name }`);
+}
+
+const displayGreetingsWithEmoji = function(name, year){
+    console.log(`😊😊Happy new year ${year} ${name}😊😊`)
+}
+
+const greet = function(name, year, func){
+    func(name, year);
+}
+
+greet('Sam', 2022, displayGreetingsWithEmoji);
+greet('Jane', 2022, displayGreetings);
